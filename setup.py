520201b858baef70858ddbf8517ae690c52a5a08
Saahil Ali <programmer290399@gmail.com>
2021-09-26 23:07:07 +0530

    setup: added extra requirements code

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from os import path
-from setuptools import setup, find_packages
+import re
 import sys
 import versioneer
+from os import path
+from pathlib import Path
+from collections import defaultdict
+from setuptools import setup, find_packages
 
 
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
@@ -19,49 +22,77 @@ This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
 Upgrade pip like so:
 
 pip install --upgrade pip
-""".format(*(sys.version_info[:2] + min_version))
+""".format(
+        *(sys.version_info[:2] + min_version)
+    )
     sys.exit(error)
 
-here = path.abspath(path.dirname(__file__))
+here = Path(__file__).parent.resolve()
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+with open(here / "README.rst", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(here / "requirements.txt") as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
+    requirements = [
+        line
+        for line in requirements_file.read().splitlines()
+        if not line.startswith("#")
+    ]
+
+
+def get_extra_requires(path, add_all=True):
+    # Helper function to parse extra dependencies from extra-requirements.txt file
+    # Source: https://hanxiao.io/2019/11/07/A-Better-Practice-for-Managing-extras-require-Dependencies-in-Python/
+    with open(path) as fp:
+        extra_deps = defaultdict(set)
+        for k in fp:
+            if k.strip() and not k.startswith("#"):
+                tags = set()
+                if ":" in k:
+                    k, v = k.split(":")
+                    tags.update(vv.strip() for vv in v.split(","))
+                tags.add(re.split("[<=>]", k)[0])
+                for t in tags:
+                    extra_deps[t].add(k)
+
+        # add tag `all` at the end
+        if add_all:
+            extra_deps["all"] = set(vv for v in extra_deps.values() for vv in v)
+
+    return extra_deps
 
 
 setup(
-    name='pyqna',
+    name="pyqna",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="A simple python package for question answering",
     long_description=readme,
     author="Saahil Ali",
-    author_email='programmer290399@gmail.com',
-    url='https://github.com/programmer290399/pyqna',
-    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
-    packages=find_packages(exclude=['docs', 'tests']),
+    author_email="programmer290399@gmail.com",
+    url="https://github.com/programmer290399/pyqna",
+    python_requires=">={}".format(".".join(str(n) for n in min_version)),
+    packages=find_packages(exclude=["docs", "tests"]),
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # 'command = some.module:some_function',
         ],
     },
     include_package_data=True,
     package_data={
-        'pyqna': [
+        "pyqna": [
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
         ]
     },
     install_requires=requirements,
+    extra_requires=get_extra_requires(here / "extra-requirements.txt"),
     license="BSD (3-clause)",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
     ],
 )
