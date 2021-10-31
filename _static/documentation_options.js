a849c04e2c21563774b1c01a0943a4e71571338a
programmer290399 <programmer290399@users.noreply.github.com>
2021-10-31 06:34:43 +0000

    deploy: 0842c13a6323e90fe90e46fbea046112160d945d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g87f6f64',
+    VERSION: '0.post1+g0842c13',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
