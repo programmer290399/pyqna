9ca63777d30dcfc5ea5c11957c15e0891ddd4ee5
Saahil Ali <programmer290399@gmail.com>
2021-11-30 12:50:30 +0530

    tests: models: rc: transformets: upadted tests

--- pyqna/tests/models/reading_comprehension/test_transformer_models.py
+++ pyqna/tests/models/reading_comprehension/test_transformer_models.py
@@ -8,12 +8,11 @@ def test_model_inference():
     model = TransformerQnAModel(
         {"model_name": "distilbert-base-uncased-distilled-squad", "pre_trained": True}
     )
-    assert (
-        model.get_answer(
-            "New Zealand (Māori: Aotearoa) is a sovereign island country in the southwestern Pacific Ocean.\
+    answers = model.get_answer(
+        "New Zealand (Māori: Aotearoa) is a sovereign island country in the southwestern Pacific Ocean.\
                 It has a total land area of 268,000 square kilometres (103,500 sq mi), and a population of\
                 4.9 million. New Zealand's capital city is Wellington, and its most populous city is Auckland.",
-            ["How many people live in New Zealand?", "What's the largest city?"],
-        )
-        == ["4. 9 million", "auckland"]
+        ["How many people live in New Zealand?", "What's the largest city?"],
     )
+    assert answers[0]["answer"] == "4. 9 million"
+    assert answers[1]["answer"] == "auckland"
